24877a2ef5bf83b83fc0b3a932a30678b31cd9c9
DEBAYAN MAL <debayanmal2002official@gmail.com>
2026-04-15 18:49:40 +0530

    Update main_app.py

--- app/main_app.py
+++ app/main_app.py
@@ -149,8 +149,6 @@ with tab2:
         "Select Segmentation Dimension",
         ["CreditScoreGroup", "AgeGroup", "SalaryGroup", "TenureGroup"]
     )
-    churn_segment = (df.groupby(segment_option)["Exited"].mean().reset_index())
-    churn_segment["Exited"] = churn_segment["Exited"] * 100
 
     # Custom ordering for known groups
     order_map = {
@@ -192,15 +190,22 @@ with tab2:
         }
     }
 
+    churn_segment = (
+    df.groupby(segment_option)["Exited"]
+    .mean()
+    .reset_index()
+)
+
+    churn_segment["Exited"] *= 100
+    churn_segment = churn_segment.dropna()
     if segment_option in order_map:
-    churn_segment[segment_option] = pd.Categorical(
-        churn_segment[segment_option],
-        categories=order_map[segment_option],
-        ordered=True
-    )
+        churn_segment[segment_option] = pd.Categorical(
+            churn_segment[segment_option],
+            categories=order_map[segment_option],
+            ordered=True
+        )
 
     churn_segment = churn_segment.sort_values(segment_option)
-    churn_segment = churn_segment.dropna()
 
     current_color_map = color_maps.get(segment_option, {})
 
