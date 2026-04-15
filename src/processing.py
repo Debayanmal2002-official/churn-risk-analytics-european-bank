27b28d1f2d9fcbf19186deee18f52a8c77ed37f3
DEBAYAN MAL <debayanmal2002official@gmail.com>
2026-04-15 18:26:58 +0530

    Update processing.py

--- src/processing.py
+++ src/processing.py
@@ -20,7 +20,7 @@ cols1 = ['Geography', 'Gender', 'Tenure', 'NumOfProducts',
 for col in cols1:
     print(f"{col}: {df[col].unique()}")
 
-cols2 = ['CreditScore', 'Age', 'EstimatedSalary']
+cols2 = ['CreditScore', 'Age', 'EstimatedSalary','Tenure','Balance']
 print(df[cols2].describe())
 
 
@@ -31,27 +31,61 @@ print(df[cols2].describe())
 df['CreditScoreGroup'] = pd.cut(
     df['CreditScore'],
     bins=[300, 580, 720, 850],
-    labels=['Low', 'Medium', 'High']
+    labels=[
+        'Low (300–580)',
+        'Medium (580–720)',
+        'High (720+)'
+    ]
 )
 # Age Segmentation
 df['AgeGroup'] = pd.cut(
     df['Age'],
     bins=[0, 30, 45, 60, 100],
-    labels=['Young', 'Mid-age', 'Mature', 'Senior']
+    labels=[
+        'Young (0–30)',
+        'Mid-age (30–45)',
+        'Mature (45–60)',
+        'Senior (60+)'
+    ]
 )
 # Salary Segmentation
 df['SalaryGroup'] = pd.cut(
     df['EstimatedSalary'],
     bins=[0, 50000, 150000, 200000],
-    labels=['Low', 'Medium', 'High']
+    labels=[
+        'Low (0–50K)',
+        'Medium (50K–150K)',
+        'High (150K+)'
+    ]
+)
+# Tenure Group
+df['TenureGroup'] = pd.cut(
+    df['Tenure'],
+    bins=[-1, 3, 7, 10],
+    labels=[
+        'New (0–3 yrs)',
+        'Mid-term (4–7 yrs)',
+        'Long-term (8+ yrs)'
+    ]
+)
+# Balance Group
+df['BalanceGroup'] = pd.cut(
+    df['Balance'],
+    bins=[-1, 0, 100000, 300000],
+    labels=[
+        'Zero (0)',
+        'Low (1–100K)',
+        'High (100K+)'
+    ]
 )
 
-for col in ['CreditScoreGroup', 'AgeGroup', 'SalaryGroup']:
+for col in cols2:
     print(df[col].value_counts())
 
 print(df.info())
 print(df.isnull().sum())
 
 ## -- Final Save --
+
 save_path = BASE_DIR / "data" / "processed_data.xlsx"
 df.to_excel(save_path, index=False)
