f1e5238da487cb6b3b3b5c23c2e99eeb96a0ceb6
DEBAYAN MAL <debayanmal2002official@gmail.com>
2026-05-16 10:09:21 +0530

    Update main_app.py

--- app/main_app.py
+++ app/main_app.py
@@ -99,39 +99,15 @@ non_zero_avg_balance = df[df["Balance"] > 0]["Balance"].mean()
 zero_balance_pct = (df["Balance"] == 0).mean() * 100
 
 # --- KPI Card Function ---
+import streamlit as st
+
 def kpi_card(title, value):
-
     card_html = f"""
-    <div style='
-        background-color:#111827;
-        padding:20px;
-        border-radius:16px;
-        border:1px solid #334155;
-        text-align:center;
-        box-shadow:0 4px 12px rgba(0,0,0,0.25);
-    '>
-
-        <p style='
-            font-size:15px;
-            color:#CBD5E1;
-            margin-bottom:10px;
-            font-weight:500;
-        '>
-            {title}
-        </p>
-
-        <h1 style='
-            font-size:34px;
-            font-weight:700;
-            color:#F8FAFC;
-            margin:0;
-        '>
-            {value}
-        </h1>
-
+    <div style="background-color:#111827; padding:20px; border-radius:16px; border:1px solid #334155; text-align:center; box-shadow:0 4px 12px rgba(0,0,0,0.25);">
+        <p style="font-size:15px; color:#CBD5E1; margin-bottom:10px; font-weight:500; margin-top:0;">{title}</p>
+        <h1 style="font-size:34px; font-weight:700; color:#F8FAFC; margin:0;">{value}</h1>
     </div>
     """
-
     st.markdown(card_html, unsafe_allow_html=True)
 
 # --- First Row ---
